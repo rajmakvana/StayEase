cf95581fd18f3867e3e7b0eb9234457bbb94ff4d
rajmakvana143 <rajmakvana143e@gmail.com>
2025-05-03 17:44:47 +0530

    authorazation added

--- middleWare.js
+++ middleWare.js
@@ -1,3 +1,7 @@
+const Listing = require('./models/listings');
+const Review  = require('./models/review');
+
+
 module.exports.isLogedIn = (req , res ,next) => {
     if(!req.isAuthenticated()){
         req.session.redirectUrl = req.originalUrl;
@@ -14,4 +18,24 @@ module.exports.redirectSaveUrl = (req , res , next) => {
         res.locals.redirectUrl = req.session.redirectUrl
     }
     next();
+}
+
+module.exports.isOwner = async (req , res , next) => {
+    let {id} = req.params ;
+    let listing = await Listing.findById(id)
+    if(!listing.owner.equals(res.locals.currUser._id)){
+        req.flash("error" , "you don't have a permision to delete Listing ");
+        return res.redirect(`/Listings/${id}`);
+    }
+    next()
+}
+
+module.exports.isReviewAuthor = async (req , res , next) => {
+    let {id , reviewId} = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error" , "you are not the author of this reviews");
+        return res.redirect(`/Listings/${id}`);
+    }  
+    next()
 }
